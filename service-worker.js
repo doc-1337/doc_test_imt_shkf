39b812b5f4263803d8721c89cc9282d7f67c4b8b
doc-1337 <semenkleshko2@gmail.com>
2026-05-24 13:09:11 +0300

    service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,38 +1,31 @@
-const CACHE_NAME = 'amputation-calc-v1';
+const CACHE_NAME = 'amputation-calc-v2';
 const urlsToCache = [
-  '/',
-  '/index.html',
-  // Додайте інші ресурси, якщо вони є (наприклад, стилі, шрифти)
+  './',
+  './index.html',
+  './manifest.json',
+  './icons/launchericon-72x72.png',
+  './icons/launchericon-96x96.png',
+  './icons/launchericon-144x144.png',
+  './icons/launchericon-192x192.png',
+  './icons/launchericon-512x512.png'
 ];
 
-// Встановлення service worker – кешування файлів
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
 });
 
-// Відповідь на запити – спочатку мережа, потім кеш (офлайн)
 self.addEventListener('fetch', event => {
   event.respondWith(
-    fetch(event.request)
-      .catch(() => caches.match(event.request))
+    fetch(event.request).catch(() => caches.match(event.request))
   );
 });
 
-// Оновлення service worker – видалення старого кешу
 self.addEventListener('activate', event => {
-  const cacheWhitelist = [CACHE_NAME];
   event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cacheName => {
-          if (!cacheWhitelist.includes(cacheName)) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
+    caches.keys().then(keys => Promise.all(
+      keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+    ))
   );
 });
